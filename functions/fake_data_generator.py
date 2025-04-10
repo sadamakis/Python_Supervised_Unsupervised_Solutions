d6868d0f0b42316b2ed92c42b5276b2f24963854
zion_1234 <creeper_8282@hotmail.com>
2025-04-10 15:23:17 +0300

    Update fake data generator to include number of features, number of redundant features, number of repeated features, noise for unsupervised modeling.

--- functions/fake_data_generator.py
+++ functions/fake_data_generator.py
@@ -17,10 +17,14 @@ def fake_data_generator(
     target_1_probability, #Float, takes values 0-1 Relevant if the target variable is binary: provide the percentage of the 1s in the. target variable. If the target is numeric, then you can leave this argument blank.
     sample_size, #Integer. Number of records in the output dataset
     predictors, #Integer. Number of predictors in the output dataset.
-    pct_informative_predictors, #Float Percentage of informative predictors - takes values between 0 and 1
-    bias_var, #Float. The bias term in the underlying linear model, 0 means no bias. bias var=0 and noise_var=0 means perfect correlation, e.g. R 2-/Gini =
-    noise_var, #Float. The standard deviation of the gaussian noise applied to the output, 0 means no noise applied to the output. 
-    weight_var, # String. weight_variable information: '1' returns a vector with 19, random returns a vector with random weights. 
+    n_informative_predictors, #Float Percentage of informative predictors - takes values between 0 and 1
+    bias_var, #Float. Applicable when target_type='n'. The bias term (constant in the regression equation) in the underlying linear model, 0 means no bias. bias var=0 and noise_var=0 means perfect correlation, e.g. R^2/Gini = 1
+    noise_var, #Float. Applicable when target_type='n'. The standard deviation of the gaussian noise applied to the output, 0 means no noise applied to the output. 
+    n_redundant, # Integer. Applicable when target_type='b'. The number of redundant features. These features are generated as random linear combinations of the informative features.
+    n_repeated, # Integer. Applicable when target_type='b'. The number of duplicated features, drawn randomly from the informative and the redundant features.
+    flip_y, # Float. Applicable when target_type='b'. Noise level (percentage of randomly flipped labels). The fraction of samples whose class is assigned randomly. Larger values introduce noise in the labels and make the classification task harder. 
+    class_sep, # Float. Applicable when target_type='b'. Class separation (higher = more separable). The factor multiplying the hypercube size. Larger values spread out the clusters/classes and make the classification task easier.
+    weight_var, # String. weight_variable information: '1' returns a vector with 1, random returns a vector with random weights. 
     seed #Integer. set the seed so that the fake dataset will be reproducible.
     ):
 
@@ -29,7 +33,7 @@ def fake_data_generator(
         test_data = make_regression (
             n_samples = sample_size
             , n_features = predictors
-            , n_informative = round (pct_informative_predictors*predictors)
+            , n_informative = n_informative_predictors
             , n_targets = 1
             , bias = bias_var
             , noise = noise_var
@@ -39,9 +43,13 @@ def fake_data_generator(
         test_data = make_classification (
             n_samples = sample_size
             , n_features = predictors
-            , n_informative = round (pct_informative_predictors*predictors) 
+            , n_informative = n_informative_predictors
             , n_classes = 2
             , weights = [1-target_1_probability, target_1_probability] 
+            , n_redundant = n_redundant
+            , n_repeated = n_repeated
+            , flip_y = flip_y
+            , class_sep = class_sep
             , random_state = seed
         )
     print('This sklearn sample took %.2fs. to run'%(time.time()-start_time_initial)) 
@@ -51,13 +59,12 @@ def fake_data_generator(
     x_data = x_data.add_prefix ('random_var_')
     fcols = x_data.select_dtypes ('float').columns
     x_data[fcols]= x_data.apply (pd.to_numeric, downcast='float') #reducing dataframe size
+    x_data = x_data.copy()
 
     #Setting Seed
     np.random. seed (seed)
     #Categorical variable creation cat_3, cat_5, cat_20, cat_200
     cat_3 = createList (1,3)
-
-
     x_data['cat_3'] = np.array(np.random.choice (cat_3, size =sample_size), dtype=np.int32)
     cat_5 = createList (1,5)
     x_data['cat_5'] = np.array(np.random.choice (cat_5, size =sample_size), dtype=np.int32)
